18f768e75f778bd1363ae802aab4df86313ca158
davidagitlen <davidagitlen@gmail.com>
2019-07-25 20:20:51 -0600

    Set up basic files, add hotel class tests and some methods

--- src/Hotel.js
+++ src/Hotel.js
@@ -1,5 +1,49 @@
 class Hotel {
-	constructor() {
-		this.test = 'testing!';
+	constructor(users, rooms, bookings, roomServices) {
+		this.users = users;
+		this.rooms = rooms;
+		this.bookings = bookings;
+		this.roomServices = roomServices;
+		this.currentDate = this.getCurrentDate();
 	}
+
+	getCurrentDate() {
+		let today = new Date();
+		let year = today.getFullYear();
+		let month = String(today.getMonth() + 1).padStart(2, '0');
+		let day = String(today.getDate()).padStart(2, '0');
+		return `${year}/${month}/${day}`
+	}
+
+	getTodaysBookings(date) {
+		return this.bookings.filter(booking => booking.date === date);
+	}
+
+	getTodaysRoomServices(date) {
+		return this.roomServices.filter(service => service.date === date);
+	}
+
+	getUnoccupiedRooms(date) {
+		let todaysBookings = this.bookings.filter(booking => booking.date === date);
+		let availableRooms = this.rooms.filter(room => 
+			!todaysBookings.some(booking => booking.roomNumber === room.number));
+		return availableRooms;
+	}
+
+	getTotalRevenue(date) {
+		let todaysBookings = this.getTodaysBookings(date);
+		let todaysRoomServices = this.getTodaysRoomServices(date);
+		let takenRooms = this.rooms.filter(room => 
+			todaysBookings.some(booking => booking.roomNumber === room.number));
+		let roomServicesTotal = todaysRoomServices.reduce((tab, service) => {
+			return tab + service.totalCost;
+		}, 0);
+		let roomTotal = takenRooms.reduce((bill, room) => {
+			return bill + room.costPerNight;
+		}, 0);
+		return roomServicesTotal + roomTotal
+	}
+
 }
+
+export default Hotel;
---
+++ test/Hotel-test.js
@@ -0,0 +1,79 @@
+import Hotel from '../src/Hotel';
+import mockData from '../src/mock-data.js';
+import chai from 'chai';
+import spies from 'chai-spies';
+
+chai.use(spies);
+
+const expect = chai.expect;
+const spy = chai.spy();
+
+let hotel = new Hotel(mockData.users, mockData.rooms, mockData.bookings, mockData.roomServices);
+let evenRooms = mockData.rooms.filter(room => room.number % 2 === 0)
+let oddRoomsBooked = mockData.bookings.filter(booking => booking.roomNumber % 2)
+
+let setDate;
+
+describe('Hotel', () => {
+	// beforeEach(() => {
+	// 	setDate = Date;
+	// 	Date = () => {
+	// 		return new setDate('07/25/2019');
+	// 	}
+	// });
+
+	it('should be a function', () => {
+		expect(Hotel).to.be.a('function');
+	});
+
+	it('should be an instance of Hotel', () => {
+		expect(hotel).to.be.an.instanceof(Hotel);
+	});
+
+	it('should store all users', () => {
+		expect(hotel.users).to.eql(mockData.users);
+	});
+
+	it('should store all rooms', () => {
+		expect(hotel.rooms).to.eql(mockData.rooms);
+	});
+
+	it('should store all bookings', () => {
+		expect(hotel.bookings).to.eql(mockData.bookings);
+	});
+
+	it('should store all room services', () => {
+		expect(hotel.roomServices).to.eql(mockData.roomServices);
+	});
+
+	describe('getCurrentDate', () => {
+		it.skip('should return today\'s date in the proper format', () => {
+			expect(hotel.getCurrentDate()).to.equal('2019/07/25')
+		});
+	});
+
+	describe('getTodaysBookings', () => {
+		it('should return today\'s booked rooms', () => {
+			expect(hotel.getTodaysBookings('2019/07/25')).to.eql(oddRoomsBooked)
+		});
+	});
+
+	describe('getTodaysRoomServices', () => {
+		it('should return room services ordered today', () => {
+			expect(hotel.getTodaysRoomServices('2019/07/25')).to.eql(mockData.roomServices.slice(0,3))
+		});
+	});
+
+	describe('getUnoccupiedRooms', () => {
+		it('should return total unoccupied rooms for today\'s date', () => {
+			expect(hotel.getUnoccupiedRooms('2019/07/25')).to.eql(evenRooms)
+		});
+	});
+
+	describe('getTotalRevenue', () => {
+		it('should return total revenue for today\'s date', () => {
+			expect(hotel.getTotalRevenue('2019/07/25')).to.equal(2287.35)
+		});
+	});
+
+})
